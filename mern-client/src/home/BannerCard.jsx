e643456e830b9d549c1637c3599e09305db08d46
ALIGHT-RW <tayseeradam25@gmail.com>
2025-03-29 17:17:15 +0200

    update in banner

--- mern-client/src/home/BannerCard.jsx
+++ mern-client/src/home/BannerCard.jsx
@@ -20,10 +20,10 @@ const BannerCard = () => {
         className="mySwiper"
         >
             <SwiperSlide><img src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1637602267i/58891551.jpg" alt="" /></SwiperSlide>
-            <SwiperSlide>https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1652105350i/59575939.jpg</SwiperSlide>
-            <SwiperSlide>https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1639230960i/58231065.jpg</SwiperSlide>
-            <SwiperSlide>hhttps://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1649228846i/59366244.jpg</SwiperSlide>
-            <SwiperSlide>https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1651600548i/58733693.jpg</SwiperSlide>
+            <SwiperSlide><img src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1652105350i/59575939.jpg" alt="" /></SwiperSlide>
+            <SwiperSlide><img src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1639230960i/58231065.jpg" alt="" /></SwiperSlide>
+            <SwiperSlide><img src="hhttps://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1649228846i/59366244.jpg" alt="" /></SwiperSlide>
+            <SwiperSlide><img src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1651600548i/58733693.jpg" alt="" /></SwiperSlide>
         </Swiper>
     </div>
   )
